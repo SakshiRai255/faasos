c33c1e2fd5026a6bbebf92b89a94c36dc10969de
mukul <mukulrajput9675@gmail.com>
2021-07-22 01:05:02 +0530

    footer soical tag active

--- scripts/checkOut.js
+++ scripts/checkOut.js
@@ -1,3 +1,20 @@
 
 
-console.log("yes")
+
+
+document.getElementById('demo1').addEventListener('click', function () {
+    
+    window.location.href = "https://www.facebook.com/Faasos";
+});
+
+
+document.getElementById("demo2").addEventListener("click", function () {
+
+     window.location.href = "https://twitter.com/faasos";
+});
+
+
+document.getElementById("demo3").addEventListener("click", function () {
+
+     window.location.href = "https://www.instagram.com/faasos/";
+});
