179b490630c01f8ea473603d59853f093c7225cf
Chiranjeev <6258860@gmail.com>
2021-08-25 01:36:19 +0530

    completed models and controllers, ready for frontend staging

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,6 +8,12 @@ const FoodItem = require("../models/foodItem.model")
 const genericController = require("./generic.controller")
 
 router.get("/", genericController(User).getAll);
+
+router.get("/:id", async (req, res) => {
+	const item = await User.findById(req.params.id).populate("orders");
+	return res.status(200).json({ item });
+})
+
 router.get("/:id", genericController(User).getOne);
 router.post("/", genericController(User).post);
 router.patch("/:id", genericController(User).updateOne);
