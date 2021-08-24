aed1fa0508d9edbd04984dd6791cb99f43e98357
Chiranjeev <6258860@gmail.com>
2021-08-24 23:51:58 +0530

    user database working

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,13 @@ const router = express.Router();
 const User = require('../models/user.model');
 const FoodItem = require("../models/foodItem.model")
 
+const genericController = require("./generic.controller")
+
+router.get("/", genericController(User).getAll);
+router.get("/:id", genericController(User).getOne);
+router.post("/", genericController(User).post);
+router.patch("/:id", genericController(User).updateOne);
+router.delete("/:id", genericController(User).deleteOne);
 
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ const userController = require("./controllers/user.controller");
 const foodItemController = require("./controllers/foodItem.controller")
 
 const app = express();
+
 app.use(express.json());
 
 app.use("/users", userController);
