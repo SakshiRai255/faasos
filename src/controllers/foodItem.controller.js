6ea0ea0e7ab9187ed3be7a8a9d15058f3ccc2543
Chiranjeev <6258860@gmail.com>
2021-08-24 23:12:50 +0530

    added generic controller

---
+++ src/controllers/foodItem.controller.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+// Getting models
+const User = require('../models/user.model');
+const FoodItem = require("../models/foodItem.model")
+
+
+
+module.exports = router;
