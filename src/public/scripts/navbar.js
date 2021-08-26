a7a689cbf02239d4e6d8afdbbf00ccbade8418b0
Chiranjeev <6258860@gmail.com>
2021-08-27 01:11:16 +0530

    product page route working

--- src/public/scripts/navbar.js
+++ src/public/scripts/navbar.js
@@ -4,10 +4,10 @@ let user = JSON.parse(localStorage.getItem('loggedUser'));
 displayName.innerText = `Hi ${user.name}`;
 
 let navLocation = document.getElementById("navLocation");
-navLocation.innerHTML = `<p>${localStorage.getItem("location")}</p><p>${localStorage.getItem("location")}, India</p>`
+navLocation.innerHTML = `<p>${localStorage.getItem("location")}</p><p>${localStorage.getItem("location")}</p>`
 
 
 let headingLocation = document.getElementById("headingLocation");
 if (headingLocation) {
-	headingLocation.innerText = `${localStorage.getItem("location")}, India`;
+	headingLocation.innerText = `${localStorage.getItem("location")}`;
 }
