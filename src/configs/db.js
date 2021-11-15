5f067c2379e4dc0ae941155b19c1de1d8a76a92e
Shahnawaz Malek <86410031+Malek1117@users.noreply.github.com>
2021-11-15 13:18:22 +0530

    Update on config/db file

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 
 const connectDB = () => {
-	return mongoose.connect(`mongodb+srv://faasos:${process.env.DATABASE_PASS}@cluster0.onmc8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
+	return mongoose.connect(`mongodb+srv://shahnawaz:${process.env.DATABASE_PASS}@cluster0.qvcok.mongodb.net/fossosDatabase?retryWrites=true&w=majority`, {
 		useNewUrlParser: true,
 		useCreateIndex: true,
 		useUnifiedTopology: true,
@@ -11,3 +11,5 @@ const connectDB = () => {
 }
 
 module.exports = connectDB;
+
+// Chiranjeev Database ==> mongodb+srv://faasos:${process.env.DATABASE_PASS}@cluster0.onmc8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
